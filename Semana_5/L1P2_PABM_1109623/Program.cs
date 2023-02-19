976088a1d79c8c9de666e2fc18748923e9ad79d7
PabloBocel <123261458+PabloBocel@users.noreply.github.com>
2023-02-18 23:04:42 -0600

    addfiles

---
+++ Semana_5/L1P2_PABM_1109623/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace L1P2_PABM_1109623
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mi segundo programa");
+            string nombre = "";
+            double edad = 0;
+            string carrera = "";
+            string carné;
+
+
+            Console.WriteLine("Ingresa tus datos");
+            Console.WriteLine("Ingresa tu nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingresa tu carrera");
+            carrera = Console.ReadLine();
+            Console.WriteLine("Ingresa tu edad");
+            edad = Convert.ToDouble( Console.ReadLine());
+            Console.WriteLine("Ingresa tu carné");
+            carné = Console.ReadLine();
+            // que bendición 
+            Console.WriteLine("Mi segundo programa");
+            Console.WriteLine("Hola " + nombre + "\ntu carrera es "+ carrera + "\ntu edad es "+ edad + "\ntu carné es "+ carné);
+
+            Console.WriteLine("Soy " + nombre + ", tengo " + edad + " años y estudio la carrera de " + carrera + " mi número de carné es " + carné);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Semana_5/L1_PABM_1109623/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace L1_PABM_1109623
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Hola clase");
+            Console.WriteLine("Soy Pablo");
+
+            Console.WriteLine("\nIngresa tu nombre");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Hola mundo");
+            if (nombre == ""){ 
+                Console.WriteLine("Nombre no valido");
+            }else
+            {
+            Console.WriteLine("Hola "+ nombre +" Como estas?");
+            }
+            Console.ReadKey(); 
+
+        }
+    }
+}
